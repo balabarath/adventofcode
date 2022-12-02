ac464052364d4077d88d6322ea0782b31eff1a13
balabarath <bagopila@gmail.com>
2022-12-03 01:33:51 +0530

    day1 part2 solution.

--- src/main/kotlin/2022/day1.kt
+++ src/main/kotlin/2022/day1.kt
@@ -2,7 +2,7 @@ package `2022`
 
 import java.io.File
 
-fun main(args: Array<String>) {
+fun part1() {
     var counter = 0
     val maxSum = File(AOCConstants.filePath("day1.txt"))
         //readline by line
@@ -16,4 +16,26 @@ fun main(args: Array<String>) {
         // return max of each batch
         .maxOf { it }
     println(maxSum)
+}
+
+fun part2() {
+    var counter = 0
+    val maxSum = File(AOCConstants.filePath("day1.txt"))
+        //readline by line
+        .readLines()
+        //assign one index++ per group till we get a blank line and group them into batches
+        .groupBy { if (it.isBlank()) counter++ else counter }
+        //convert map to list
+        .values
+        // find sum for each batch
+        .map { it -> it.map { it.toIntOrNull() ?: 0 }.sumOf { it } }
+        // return max of each batch
+        .sortedDescending()
+        .take(3)
+        .sum()
+    println(maxSum)
+}
+
+fun main() {
+    part2()
 }
